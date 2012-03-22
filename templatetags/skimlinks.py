69a0a08177dcc61773903917a4f46d603c4a6512
PirosB3 <pirosb3@gmail.com>
2012-03-22 13:20:43 +0000

    added complete href

--- templatetags/skimlinks.py
+++ templatetags/skimlinks.py
@@ -6,23 +6,39 @@ from django import template
 SKIMLINK_API = "http://go.redirectingat.com/"
 
 class SkimlinksLinkNode(template.Node):
-    def __init__(self, link, product_id):
-        self.link = urllib.quote_plus(link)
+    def __init__(self, text, link, product_id):
+        self.text = text
         self.product_id = product_id
+        self.link = urllib.quote_plus(link)
 
-    def render(self, context):
+    def generate_link(self):
         return "".join([SKIMLINK_API, "?id=", self.product_id, "&url=",
             self.link, "&xs=1"])
+
+    def render(self, context):
+        return '<a rel="nofollow" href="%s">%s</a>' % (self.generate_link(),
+                self.text)
 
 def do_skim(parser, token):
     """returns an affiliate link"""
 
     # Get product ID from settings and link
     product_id = settings.SKIMLINKS_PRODUCT_ID
-    link = token.split_contents()[1]
+
+    # Get data from token
+    contents = token.split_contents()
+    if len(contents) != 3:
+        raise template.TemplateSyntaxError("Syntax incorrect")
+    text, link = contents[1:]
+
     if not product_id:
         raise template.TemplateSyntaxError("Please insert Skimlinks key")
-    return SkimlinksLinkNode(link, product_id)
+
+    # Return node
+    return SkimlinksLinkNode(
+        text.replace('"', ''),
+        link, product_id
+    )
 
 register = template.Library()
 register.tag("skim", do_skim)
